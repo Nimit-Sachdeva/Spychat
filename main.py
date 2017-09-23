340eadcfcbc637201275d255602fd7d007780447
Nimit Sachdeva <nimitsachdeva13@gmail.com>
2017-09-23 19:00:15 +0530

    Asking spy to enter spyname and salutation

---
+++ main.py
@@ -0,0 +1,8 @@
+print 'Hey spy '
+print 'What\\\'s up'
+spy_name = raw_input("Welcome to spy chat, ohh spy spy spy tera naam to btaaa: ")
+print 'Welcome ' + spy_name + '. Glad to have you back with us.'
+spy_salutation = raw_input("Should I call you Mister or Miss?: ")
+spy_name = spy_salutation + " " + spy_name
+print 'Arre deewano ' + 'Mujhe Pehchano ' + 'Kahan se aayaaaaa ' +'Main hu corn' + 'Main hu' + spy_name
+print 'Alright ' +spy_name + '. I\'d like to know a little bit more about...'
