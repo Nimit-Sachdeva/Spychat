130daa251d4817f28a2beb434f7add73ca4b10e4
Nimit Sachdeva <nimitsachdeva13@gmail.com>
2017-09-27 01:54:13 +0530

    Start chat function is defined for menu

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
+from spy_details import spy_name, spy_salutation, spy_age, spy_rating
+
 print 'Hey spy '
 print 'What\\\'s up'
-spy_name = raw_input("Welcome to spy chat, ohh spy spy spy tera naam to btaaa: ")
-# spy_name will take name of the user as input of the spy
-if len(spy_name)>0:             #the user should give his name input
-    spy_age= 0
-    spy_rating= 0.0
-    spy_online= False
-    spy_age= input('Kitne saalo se bojh ho is duniya pe ?')
-    if spy_age > 12 and spy_age<50:
+
+question = "Continue as " + spy_salutation + " " + spy_name + "(Y/N)?"
+existing = raw_input(question)
+
+
+def start_chat(spy_name, spy_age, spy_rating):
+    current_status_message = None
+    spy_name = spy_salutation + " " + spy_name
+    if spy_age > 12 and spy_age < 50:
+        print 'Authentication complete, Aaja mere sher, check krle,\n Naam hai: ' + spy_name + '\n Umar hai : ' + str(
+            spy_age) + '\n Rating hai:' + str(spy_rating) + '\n Chl fir aage bdho'
+        # show_menu = True
+
+        # while show_menu:
+    # Create on your own
+    else:
+        print 'tu chhod de moh maya tumse na ho paega '
+
+    print 'Welcome %s. Apse milkr khushi hui.' % spy_name
+    # we can also write %s instead of ' '+ variable
+
+
+if existing == "Y":
+    start_chat(spy_name, spy_age, spy_rating)
+else:
+    spy_name = ''
+    spy_salutation = ''
+    spy_age = 0
+    spy_rating = 0.0
+    spy_is_online = False
+
+    spy_name = raw_input("Welcome to spy chat, ohh spy spy spy tera naam to btaaa: ")
+    if len(spy_name) > 0:  # the user should give his name input
+        # asking if he is a male or female
+        spy_salutation = raw_input("Should I call you Mister or Miss?: ")
+
+        print 'Arre deewano ' + 'Mujhe Pehchano ' + 'Kahan se aayaaaaa ' + 'Main hu corn' + ' Main hu ' + spy_name
+        print 'Alright ' + spy_name + '. Aur bta apne baare me fir  '
+        spy_age = input('Kitne saalo se bojh ho is duniya pe ?')
+        # if spy_age > 12 and spy_age < 50:
         spy_rating = input('What is your rating?')
         print 'Arre bhai tu to ekdm fit hai spy bnne k liye'
-    else:
-        print 'Budha ho gya hai tu chhod de moh maya aur sanyasi bnja '
-    print 'jb ye aega garmi thodi bdh jaegi'
-    print 'Welcome ' + spy_name + '. Apse milkr khushi hui.'
-    spy_salutation = raw_input("Should I call you Mister or Miss?: ")
-    #asking if he is a male or female
-    spy_name = spy_salutation + " " + spy_name
-    print 'Arre deewano ' + 'Mujhe Pehchano ' + 'Kahan se aayaaaaa ' +'Main hu corn' + ' Main hu' + spy_name
-    print 'Alright ' +spy_name + '. Aur bta apne baare me fir  '
-else:                       #If spy didnt give any input
-    print 'Oh nadaaan parinde naam bata jaa'
-if spy_rating >=4.5:
-    print 'Tu hi to hai hmara best spy'
-elif spy_rating >=3.5 and spy_rating<4.5:
-    print 'chl chl ache se kaam kia kr'
-elif spy_rating >=2.5 and spy_rating<3.5:
-    print 'tujhe bhi denge kuch kaam'
-else:
-    print 'tu bhai naam dubayega'
-spy_online=True
-print 'Authentication complete, Aaja mere sher, check krle,\n Naam hai: '+spy_name+ '\n Umar hai : '+str(spy_age) +'\n Rating hai:'+str(spy_rating) +'\n Chl fir aage bdho'
+        if spy_rating >= 4.5:
+            print 'Tu hi to hai hmara best spy'
+        elif spy_rating >= 3.5 and spy_rating < 4.5:
+            print 'Acha hai pr aur ache se kaam kia kr'
+        elif spy_rating >= 2.5 and spy_rating < 3.5:
+            print 'tujhe bhi denge kuch kaam'
+        else:
+            print 'tu bhai naam dubayega'
+        spy_online = True
 
+        start_chat(spy_name, spy_age, spy_rating)
 
-
+        print 'jb ye aega garmi thodi bdh jaegi'
+    else:  # If spy didnt give any input
+        print 'Oh nadaaan parinde naam bata jaa'
---
+++ spy_details.py
@@ -0,0 +1,6 @@
+spy_name = 'Spyderman'
+spy_salutation = 'Mr.'
+spy_age = 23
+spy_rating = 4.7
+
+
