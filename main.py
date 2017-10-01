ebf0fdb4fffec802c1020aa755d992628953405b
Nimit Sachdeva <nimitsachdeva13@gmail.com>
2017-10-01 16:53:04 +0530

    Menu added to add status , add friend

--- main.py
+++ main.py
@@ -1,9 +1,62 @@
 from spy_details import spy_name, spy_salutation, spy_age, spy_rating
 
+STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.', 'Keeping the British end up, Sir']
+friends_name = []
+friends_age = []
+friends_rating = []
+friends_is_online = []
+
+
+# below function add_status is used to add status by spy just like apps like fb has
+def add_status(current_status_message):
+    if current_status_message != None:
+        print "Your current status message is " + current_status_message + "\n"
+    else:
+        print 'Abhi tk tera status message diya nahi hua koi tune...  \n'
+    default = input("Ye status pehle vala hi dena hai kya? (y/n)? ")
+    if default.upper() == "N":
+        new_status_message = input("Kuch btana chahoge apne dosto ko?")
+
+        if len(new_status_message) > 0:
+            updated_status_message = new_status_message
+            STATUS_MESSAGES.append(updated_status_message)
+    elif default.upper() == "Y":
+        item_position = 1
+        for message in STATUS_MESSAGES:
+            print item_position + " , " + message
+            # can also use print '%d. %s' % (item_position, message)
+
+            item_position = item_position + 1  # incrementing the item_position in order to check other items in list
+            message_selection = input(" Bta beta kya btana hai?? Number bol: ")
+            if len(STATUS_MESSAGES) >= message_selection:
+                updated_status_message = STATUS_MESSAGES[message_selection - 1]
+            else:
+                print 'You did not update your status message'
+            return updated_status_message
+
+
+def add_friend():
+    friend_name = raw_input("Tere jesa yaar kahaaan ?? Vese tera naam to btaaaa:")
+    friend_salutation = raw_input("Arrre ye bta tu hai kya... Mr. or Ms.?:")
+    friend_name = friend_salutation + " " + friend_name
+    friend_age = input("Kitne saalo ka hai ye friend?")
+    friend_rating = input("bta bhai teri rating bta")
+
+    if friend_age > 12 and friend_rating >= spy_rating and len(friend_name) > 0:
+        friends_name.append(friend_name)
+        friends_age.append(friend_age)
+        friends_rating.append(friend_rating)
+        friends_is_online.append(True)
+        print 'Sabhi spyjano ko soochit kiya jata hai k hme ek mitra mila hai !'
+    else:
+        print 'Tu nahi bn skta dost ....'
+    return len(friends_name)
+
+
 print 'Hey spy '
 print 'What\\\'s up'
 
-question = "Continue as " + spy_salutation + " " + spy_name + "(Y/N)?"
+question = "Continue as " + spy_salutation + " " + spy_name + "(Y/N)?"  # just to check if it is existing user or another one
 existing = raw_input(question)
 
 
@@ -13,10 +66,22 @@ def start_chat(spy_name, spy_age, spy_rating):
     if spy_age > 12 and spy_age < 50:
         print 'Authentication complete, Aaja mere sher, check krle,\n Naam hai: ' + spy_name + '\n Umar hai : ' + str(
             spy_age) + '\n Rating hai:' + str(spy_rating) + '\n Chl fir aage bdho'
-        # show_menu = True
+        show_menu = True
 
-        # while show_menu:
-    # Create on your own
+        while show_menu:
+            menu_choices = "What do you want to do? \n 1. Add a status update \n 2. Add a friend \n 3. Send a secret message \n 4. Read a secret message \n 5. Read Chats from a user \n 6. Close Application \n"
+            menu_choice = input(menu_choices)
+
+            if len(menu_choice) > 0:
+                menu_choice = int(menu_choice)
+                if menu_choice == 1:
+                    current_status_message = add_status(current_status_message)
+                elif menu_choice == 2:
+                    number_of_friends = add_friend()
+                    print 'You have %d friends' % (number_of_friends)
+                else:
+                    show_menu = False
+
     else:
         print 'tu chhod de moh maya tumse na ho paega '
 
