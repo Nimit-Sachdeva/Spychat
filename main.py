142c1179c5397dfbcf7c1d1e86e77c96632cb650
Nimit Sachdeva <nimitsachdeva13@gmail.com>
2017-10-10 22:24:46 +0530

    Added some friends and can read chats with date and time

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from datetime import datetime
 import time
+from termcolor import colored
 from steganography.steganography import Steganography
-from spy_details import spy_name, spy_salutation, spy_age, spy_rating
-
+from spy_details import spy, Spy, ChatMessage, friends
+#below are the previous statuses
 STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.', 'Keeping the British end up, Sir']
-# friends_name = []
-# friends_age = []
-# friends_rating = []
-# friends_is_online = []
-friends = []
 
+#List of special words
+SPECIAL_WORDS=['SOS','SAVE ME','NEED HELP']
 
 # below function add_status is used to add status by spy just like apps like fb has
 def add_status(current_status_message):
-    if current_status_message != None:
+    if spy.current_status_message != None:
         print "Your current status message is " + current_status_message + "\n"
     else:
         print 'Abhi tk tera status message diya nahi hua koi tune...  \n'
@@ -47,20 +45,14 @@ def add_status(current_status_message):
 
 
 def add_friend():
-    new_friend = {
-        'name': '',
-        'salutation': '',
-        'rating': 0.0,
-        'age': 0,
-        # 'online':False
-    }
-    new_friend['name'] = raw_input("Tere jesa yaar kahaaan ?? Vese tera naam to btaaaa:")
-    new_friend['salutation'] = raw_input("Arrre ye bta tu hai kya... Mr. or Ms.?:")
-    new_friend['name'] = new_friend['salutation'] + " " + new_friend['name']
-    new_friend['age'] = input("Kitne saalo ka hai ye friend?")
-    new_friend['rating'] = input("bta bhai teri rating bta")
+    new_friend = Spy ('', '', 0, 0.0)
+    new_friend.name = raw_input("Tere jesa yaar kahaaan ?? Vese tera naam to btaaaa:")
+    new_friend.salutation = raw_input("Arrre ye bta tu hai kya... Mr. or Ms.?:")
+    new_friend.name = new_friend.salutation + " " + new_friend.name
+    new_friend.age = input("Kitne saalo ka hai ye friend?")
+    new_friend.rating = input("bta bhai teri rating bta")
 
-    if new_friend['age'] > 12 and new_friend['rating'] >= spy_rating and len(new_friend['name']) > 0:
+    if new_friend.age > 12 and new_friend.rating >= spy.rating and len(new_friend.name) > 0:
         friends.append(new_friend)
         print 'Sabhi spyjano ko soochit kiya jata hai k hme ek mitra mila hai !'
     else:
@@ -71,7 +63,7 @@ def add_friend():
 print 'Hey spy '
 print 'What\\\'s up'
 
-question = "Continue as " + spy_salutation + " " + spy_name + "(Y/N)?"  # just to check if it is existing user or another one
+question = "Continue as " + spy.salutation + " " + spy.name + "(Y/N)?"  # just to check if it is existing user or another one
 existing = raw_input(question)
 
 
@@ -79,51 +71,71 @@ existing = raw_input(question)
 def select_a_friend():
     friend_item_number = 0
     for friend in friends:
-        print '%d %s aged %d having rating %2f is online' % (friend_item_number + 1, friend['name'],friend['age'],friend['rating'])
+        print '%d %s aged %d having rating %2f is online' % (friend_item_number + 1, friend.name,friend.age,friend.rating)
         friend_item_number = friend_item_number + 1
     friend_choice = input('Choose the number from your friends')
     friend_choice_position = friend_choice - 1
     return friend_choice_position
 
-
+#This module will enable spy to send the secret encoded message to another spy
+#Send message method starts from here
 def send_encoded_message():
     friend_choice = select_a_friend()
     original_image = raw_input('Abe bole to apni fotuu ka naam bta ?')
     original_path = 'Manzil.jpg'
     text = raw_input('Kya bhejna hai tujhe gupt rkhkr ..mujhe to btana pdega')
     Steganography.encode(original_image, original_path, text)
-    new_chat = {
-        'message': text,
-        'time': datetime.now(),
-        'sent_by_me': True
-    }
+    #below ChatMessage function is imported from spy_details.py
+    new_chat = ChatMessage(text, True)
     print "Spyjan kripya dhyan de, apka secret message image ready hai!"
 
-    friends[friend_choice]['chats'].append(new_chat)
+    friends[friend_choice].chats.append(new_chat)
 
+#This module will enable spy to read the secret message sent by another spy
+#Read Message method starts from here
 
 def read_message():
     sender = select_a_friend()
     output_path = raw_input('Abe chl khajoor fotuu ka naam bta')
     secret_text = Steganography.decode(output_path)
-    new_chat = {
-        "message": secret_text,
-        "time": datetime.now(),
-        "sent_by_me": False
-    }
+    new_chat = ChatMessage(secret_text,False)
+    friends[sender].chats.append(new_chat)
+    print "Abe oh kaaliaa ,Tera khana mene kha lia .....aur han Message mere pas hai re!"
+    print secret_text
 
-    # friends[sender]['chats'].append(new_chat)
+    text_length = len(secret_text) - secret_text.count(" ")
+    if text_length > 100:
+        del friends[sender]
+    if "save me".upper() in secret_text:
+        print "Spyjan zara dhyaan de ....apke ek bhai bandhu ko sahayta chahiye !"
+    if "alert" in secret_text:
+        print "Abe oh ghonchuu , Dhyan rkh "
+    if "SOS" in secret_text:
+        print "Emergency call"
+#below function is used to read chat history
+def read_chat_history():
+    read_for=select_a_friend()
+    print '\n'
+    for chat in friends[read_for].chats:
+        if chat.sent_by_me:
+            print colored('[%s]'%(chat.time.strftime('%d %B %Y')),'blue')
+            print colored('You said:%s' % chat.message, "grey")
+        else:
+            print colored('[%s]  ' % (chat.time.strftime('%d %B %Y')), "blue")
+            print colored('%s' % (friends[read_for].name), "red")
+            print colored('%s' % (chat.message), "grey")
 
 
-print "Abe oh kaaliaa , Message mere pas hai re!"
-
-
-def start_chat(spy_name, spy_age, spy_rating):
+#below is the function that is used for chat mainly
+def start_chat(spy):
     current_status_message = None
-    spy_name = spy_salutation + " " + spy_name
-    if spy_age > 12 and spy_age < 50:
-        print 'Authentication complete, Aaja mere sher, check krle,\n Naam hai: ' + spy_name + '\n Umar hai : ' + str(
-            spy_age) + '\n Rating hai:' + str(spy_rating) + '\n Chl fir aage bdho'
+    spy.name = spy.salutation + " " + spy.name
+    if spy.age > 12 and spy.age < 50:
+        print 'Authentication complete, Aaja mere sher, check krle,\n Naam hai: ' + spy.name + '\n Umar hai : ' + str(
+            spy.age) + '\n Rating hai:' + str(spy.rating) + '\n Chl fir aage bdho'
+        print 'Welcome %s. Apse milkr khushi hui.' % spy.name
+        # we can also write %s instead of ' '+ variable
+
         show_menu = True
 
         while show_menu:
@@ -142,48 +154,51 @@ def start_chat(spy_name, spy_age, spy_rating):
                 send_encoded_message()
             elif menu_choice==4:
                 read_message()
+            elif menu_choice == 5:
+                read_chat_history()
             else:
                 show_menu = False
 
     else:
         print 'tu chhod de moh maya tumse na ho paega '
-
-    print 'Welcome %s. Apse milkr khushi hui.' % spy_name
-    # we can also write %s instead of ' '+ variable
+        exit()
 
 
-if existing == "Y":
-    start_chat(spy_name, spy_age, spy_rating)
-else:
-    spy_name = ''
-    spy_salutation = ''
-    spy_age = 0
-    spy_rating = 0.0
-    spy_is_online = False
 
-    spy_name = raw_input("Welcome to spy chat, ohh spy spy spy tera naam to btaaa: ")
-    if len(spy_name) > 0:  # the user should give his name input
+
+if existing.upper() == "Y":
+    start_chat(spy)
+elif existing.upper() =="N":
+    spy = Spy('', '', 0, 0.0)
+    spy.name = raw_input("Welcome to spy chat, ohh spy spy spy tera naam to btaaa: ")
+    if len(spy.name) > 0:  # the user should give his name input
         # asking if he is a male or female
-        spy_salutation = raw_input("Should I call you Mister or Miss?: ")
-
-        print 'Arre deewano ' + 'Mujhe Pehchano ' + 'Kahan se aayaaaaa ' + 'Main hu corn' + ' Main hu ' + spy_name
-        print 'Alright ' + spy_name + '. Aur bta apne baare me fir  '
+        spy.salutation = raw_input("Should I call you Mister or Miss?: ")
+        spy.name = spy.salutation + " " + spy.name
+        print 'Arre deewano ' + 'Mujhe Pehchano ' + 'Kahan se aayaaaaa ' + 'Main hu corn' + ' Main hu ' + spy.name
+        print 'Alright ' + spy.name + '. Aur bta apne baare me fir  '
         spy_age = input('Kitne saalo se bojh ho is duniya pe ?')
-        # if spy_age > 12 and spy_age < 50:
-        spy_rating = input('What is your rating?')
-        print 'Arre bhai tu to ekdm fit hai spy bnne k liye'
-        if spy_rating >= 4.5:
-            print 'Tu hi to hai hmara best spy'
-        elif spy_rating >= 3.5 and spy_rating < 4.5:
-            print 'Acha hai pr aur ache se kaam kia kr'
-        elif spy_rating >= 2.5 and spy_rating < 3.5:
-            print 'tujhe bhi denge kuch kaam'
+        if spy_age > 12 and spy_age < 50:
+            spy.rating = input('What is your rating?')
+            print 'Arre bhai tu to ekdm fit hai spy bnne k liye'
+            if spy.rating >= 4.5:
+                print 'Tu hi to hai hmara best spy'
+            elif spy.rating >= 3.5 and spy.rating < 4.5:
+                print 'Acha hai pr aur ache se kaam kia kr'
+            elif spy.rating >= 2.5 and spy.rating < 3.5:
+                print 'tujhe bhi denge kuch kaam'
+            else:
+                print 'tu bhai naam dubayega'
+            spy_online = True
+
+            start_chat(spy)
+
+            print 'jb ye aega garmi thodi bdh jaegi'
         else:
-            print 'tu bhai naam dubayega'
-        spy_online = True
-
-        start_chat(spy_name, spy_age, spy_rating)
-
-        print 'jb ye aega garmi thodi bdh jaegi'
+            print 'tu chhod de moh maya tumse na ho paega '
+            exit()
     else:  # If spy didnt give any input
         print 'Oh nadaaan parinde naam bata jaa'
+else:
+    print 'Bol spy bol Naam hoga to koi :-)....aur jo bola utna hi enter kro'
+
--- spy_details.py
+++ spy_details.py
@@ -1,4 +1,29 @@
-spy_name = 'Spyderman'
-spy_salutation = 'Mr.'
-spy_age = 23
-spy_rating = 4.7
+from datetime import datetime
+#Creating class spy
+class Spy:
+
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
+        self.chats = []
+        self.current_status_message = None
+
+#Creating class chatmessage to read chat history
+class ChatMessage:
+
+    def __init__(self,message,sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+spy = Spy('Spyderman', 'Mr.', 20, 5.0)
+
+friend_one = Spy('Spyderwoman', 'Ms.', 27,4.9)
+friend_two = Spy('Hitler', 'Mr.', 21,4.7)
+friend_three = Spy('Rocker', 'Mr.', 37,4.9)
+
+
+friends = [friend_one, friend_two, friend_three]
